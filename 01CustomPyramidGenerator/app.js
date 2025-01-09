a9b8e2b5d916420af39fd514889e6f4285f7ab54
muhammad umair <muhammadumairabbasi637@gmail.com>
2025-01-09 15:41:20 +0500

    pyramid project 50% done

--- 01CustomPyramidGenerator/app.js
+++ 01CustomPyramidGenerator/app.js
@@ -16,3 +16,28 @@ A pyramid generator is a program where you can set the type of character, the co
 // count 5.
 // direction upword
 // logic = character increase add number which is 2  in each row, space decrease 1 in each row
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(name) {
+  const test = "Testing";
+
+  return test;
+}
+
+const call = padRow("CamperChan");
+console.log(call);
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1));
+}
+
+let result = "";
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
